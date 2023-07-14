4bb0c4af47bfb2f9e83b95dfca20e7e706a5ae4f
tetra5 <optofile@gmail.com>
2023-07-14 11:52:06 +0300

    initial commit

--- cmd/diabler/main.go
+++ cmd/diabler/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/tetra5/diabler/pkg/d4/events"
@@ -40,10 +39,11 @@ type Data struct {
 }
 
 type User struct {
-	ChatID       string    `json:"chat_id"`
-	UTCOffset    int       `json:"utc_offset,omitempty"`
-	WBAlarmTimer int       `json:"wb_alarm_timer,omitempty"`
-	WBNotifiedOn time.Time `json:"wb_notified_on,omitempty"`
+	ChatID        string    `json:"chat_id"`
+	UTCOffset     int       `json:"utc_offset,omitempty"`
+	WBAlarmTimer  int       `json:"wb_alarm_timer,omitempty"`
+	WBNotifiedOn  time.Time `json:"wb_notified_on,omitempty"`
+	MenuMessageID int       `json:"menu_message_id"`
 }
 
 func UpdateTimers(wbs *events.WorldBossSchedule, bot *tgbotapi.BotAPI) {
@@ -131,10 +131,11 @@ func GetUserIdx(d *Data, chatID int64) (idx int) {
 
 func NewUser(chatID int64) (user User) {
 	return User{
-		UTCOffset:    defaultUTCOffset,
-		WBAlarmTimer: defaultWBAlarmTimer,
-		WBNotifiedOn: time.Unix(0, 0),
-		ChatID:       strconv.FormatInt(chatID, 10),
+		UTCOffset:     defaultUTCOffset,
+		WBAlarmTimer:  defaultWBAlarmTimer,
+		WBNotifiedOn:  time.Unix(0, 0),
+		ChatID:        strconv.FormatInt(chatID, 10),
+		MenuMessageID: -1,
 	}
 }
 
@@ -153,15 +154,15 @@ func main() {
 	updateConfig.Timeout = timeout
 	log.Printf("Telegram: @%s, update timeout %s", &bot.Self, PluralizeStr(timeout, "second", "seconds", true))
 
-	wbs := events.NewWorldBossSchedule()
+	// wbs := events.NewWorldBossSchedule()
 
-	ticker := time.NewTicker(time.Second * updateInterval)
-	go func() {
-		for {
-			<-ticker.C
-			go UpdateTimers(wbs, bot)
-		}
-	}()
+	// ticker := time.NewTicker(time.Second * updateInterval)
+	// go func() {
+	// 	for {
+	// 		<-ticker.C
+	// 		go UpdateTimers(wbs, bot)
+	// 	}
+	// }()
 
 	for update := range bot.GetUpdatesChan(updateConfig) {
 		if update.Message == nil {
@@ -173,113 +174,141 @@ func main() {
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		msg.ParseMode = tgbotapi.ModeMarkdown
+		savingMessageID := false
+
+		data, err := LoadData(dataPath)
+		if err != nil {
+			log.Printf("Error loading data: %s. I make a new one!", err)
+			data = &Data{}
+		}
+		chatID := update.Message.Chat.ID
+		idx := GetUserIdx(data, chatID)
+		if idx == -1 {
+			log.Printf("User %q not found. I make a new one!", chatID)
+			data.Users = append(data.Users, NewUser(chatID))
+			idx = GetUserIdx(data, chatID)
+			err := SaveData(dataPath, data)
+			if err != nil {
+				log.Printf("Error saving data: %s", err)
+				msg.Text = DataSaveErrorStr
+			}
+		}
 
 		switch update.Message.Command() {
 		case "diabler":
-			fields := strings.Fields(update.Message.Text)
-			if len(fields) < 2 {
-				msg.Text = usageStr
-			} else {
-				data, err := LoadData(dataPath)
-				if err != nil {
-					log.Printf("Data load error: %s. I make a new one!", err)
-					data = &Data{}
-				}
+			savingMessageID = true
+			msg.Text = MainMenuStr
+			// fields := strings.Fields(update.Message.Text)
+			// if len(fields) < 2 {
+			// 	msg.Text = usageStr
+			// } else {
+			// 	data, err := LoadData(dataPath)
+			// 	if err != nil {
+			// 		log.Printf("Data load error: %s. I make a new one!", err)
+			// 		data = &Data{}
+			// 	}
 
-				chatID := update.Message.Chat.ID
-				idx := GetUserIdx(data, chatID)
-				if idx == -1 {
-					log.Printf("User %d not found. I make a new one!", chatID)
-					data.Users = append(data.Users, NewUser(chatID))
-					idx = GetUserIdx(data, chatID)
-					err = SaveData(dataPath, data)
-					if err != nil {
-						log.Printf("Data save error: %s\n", err)
-						msg.Text = DataSaveErrorStr
-					}
-				}
-				utcOffset := data.Users[idx].UTCOffset
-				fz := time.FixedZone("", 3600*utcOffset)
+			// 	chatID := update.Message.Chat.ID
+			// 	idx := GetUserIdx(data, chatID)
+			// 	if idx == -1 {
+			// 		log.Printf("User %d not found. I make a new one!", chatID)
+			// 		data.Users = append(data.Users, NewUser(chatID))
+			// 		idx = GetUserIdx(data, chatID)
+			// 		err = SaveData(dataPath, data)
+			// 		if err != nil {
+			// 			log.Printf("Data save error: %s\n", err)
+			// 			msg.Text = DataSaveErrorStr
+			// 		}
+			// 	}
+			// 	utcOffset := data.Users[idx].UTCOffset
+			// 	fz := time.FixedZone("", 3600*utcOffset)
 
-				switch fields[1] {
-				case "wb":
-					switch len(fields) {
-					case 2:
-						// Show next WB spawn time and alarm timer if set
-						rounded := RoundUpTime(wbs.Next().SpawnTime, time.Minute)
-						remaining := time.Until(rounded)
-						msg.Text = fmt.Sprintf(wbNextSpawnTimeStr,
-							wbs.Next().Name,
-							remaining.Round(time.Second).String(),
-							RoundUpTime(wbs.Next().SpawnTime.In(fz), time.Minute).Format(time.DateTime),
-							FormatUTCOffset(utcOffset),
-						)
-						var timerStr string
-						if data.Users[idx].WBAlarmTimer > 0 {
-							timerStr = fmt.Sprintf(WBTimerStr, PluralizeStr(data.Users[idx].WBAlarmTimer, "minute", "minutes", true))
-						} else {
-							timerStr = WBTimerDisabledStr
-						}
-						msg.Text = strings.Join([]string{msg.Text, timerStr}, "\n")
-					case 3:
-						// Set or remove the alarm timer
-						prevMinutes := data.Users[idx].WBAlarmTimer
-						minutes, err := strconv.Atoi(fields[2])
-						if err != nil || minutes < 0 {
-							minutes = 0
-						}
-						data.Users[idx].WBAlarmTimer = minutes
-						data.Users[idx].WBNotifiedOn = time.Unix(0, 0)
-						err = SaveData(dataPath, data)
-						if err != nil {
-							log.Printf("Data save error: %s\n", err)
-							msg.Text = DataSaveErrorStr
-						}
-						if minutes > 0 {
-							msg.Text = fmt.Sprintf(WBTimerChangedStr,
-								PluralizeStr(prevMinutes, "minute", "minutes", true),
-								PluralizeStr(minutes, "minute", "minutes", true),
-							)
-						} else {
-							msg.Text = WBTimerDisabledStr
-						}
-						log.Printf("WB alarm timer set: Chat ID=%s, time=%s", data.Users[idx].ChatID, PluralizeStr(minutes, "minute", "minutes", true))
-					default:
-						msg.Text = usageStr
-					}
+			// 	switch fields[1] {
+			// 	case "wb":
+			// 		switch len(fields) {
+			// 		case 2:
+			// 			// Show next WB spawn time and alarm timer if set
+			// 			rounded := RoundUpTime(wbs.Next().SpawnTime, time.Minute)
+			// 			remaining := time.Until(rounded)
+			// 			msg.Text = fmt.Sprintf(wbNextSpawnTimeStr,
+			// 				wbs.Next().Name,
+			// 				remaining.Round(time.Second).String(),
+			// 				RoundUpTime(wbs.Next().SpawnTime.In(fz), time.Minute).Format(time.DateTime),
+			// 				FormatUTCOffset(utcOffset),
+			// 			)
+			// 			var timerStr string
+			// 			if data.Users[idx].WBAlarmTimer > 0 {
+			// 				timerStr = fmt.Sprintf(WBTimerStr, PluralizeStr(data.Users[idx].WBAlarmTimer, "minute", "minutes", true))
+			// 			} else {
+			// 				timerStr = WBTimerDisabledStr
+			// 			}
+			// 			msg.Text = strings.Join([]string{msg.Text, timerStr}, "\n")
+			// 		case 3:
+			// 			// Set or remove the alarm timer
+			// 			prevMinutes := data.Users[idx].WBAlarmTimer
+			// 			minutes, err := strconv.Atoi(fields[2])
+			// 			if err != nil || minutes < 0 {
+			// 				minutes = 0
+			// 			}
+			// 			data.Users[idx].WBAlarmTimer = minutes
+			// 			data.Users[idx].WBNotifiedOn = time.Unix(0, 0)
+			// 			err = SaveData(dataPath, data)
+			// 			if err != nil {
+			// 				log.Printf("Data save error: %s\n", err)
+			// 				msg.Text = DataSaveErrorStr
+			// 			}
+			// 			if minutes > 0 {
+			// 				msg.Text = fmt.Sprintf(WBTimerChangedStr,
+			// 					PluralizeStr(prevMinutes, "minute", "minutes", true),
+			// 					PluralizeStr(minutes, "minute", "minutes", true),
+			// 				)
+			// 			} else {
+			// 				msg.Text = WBTimerDisabledStr
+			// 			}
+			// 			log.Printf("WB alarm timer set: Chat ID=%s, time=%s", data.Users[idx].ChatID, PluralizeStr(minutes, "minute", "minutes", true))
+			// 		default:
+			// 			msg.Text = usageStr
+			// 		}
 
-				case "utc":
-					switch len(fields) {
-					case 2:
-						// Show current UTC offset
-						msg.Text = fmt.Sprintf(UTCOffsetStr, FormatUTCOffset(utcOffset))
-					case 3:
-						// Set UTC offset
-						prevOffset := data.Users[idx].UTCOffset
-						utcOffset, err = strconv.Atoi(fields[2])
-						if err != nil {
-							utcOffset = 0
-						}
-						data.Users[idx].UTCOffset = utcOffset
-						err = SaveData(dataPath, data)
-						if err != nil {
-							log.Printf("Data save error: %s", err)
-							msg.Text = DataSaveErrorStr
-						} else {
-							msg.Text = fmt.Sprintf(UTCOffsetSetStr, FormatUTCOffset(prevOffset), FormatUTCOffset(utcOffset))
-						}
-					}
-				default:
-					msg.Text = usageStr
-				}
-			}
+			// 	case "utc":
+			// 		switch len(fields) {
+			// 		case 2:
+			// 			// Show current UTC offset
+			// 			msg.Text = fmt.Sprintf(UTCOffsetStr, FormatUTCOffset(utcOffset))
+			// 		case 3:
+			// 			// Set UTC offset
+			// 			prevOffset := data.Users[idx].UTCOffset
+			// 			utcOffset, err = strconv.Atoi(fields[2])
+			// 			if err != nil {
+			// 				utcOffset = 0
+			// 			}
+			// 			data.Users[idx].UTCOffset = utcOffset
+			// 			err = SaveData(dataPath, data)
+			// 			if err != nil {
+			// 				log.Printf("Data save error: %s", err)
+			// 				msg.Text = DataSaveErrorStr
+			// 			} else {
+			// 				msg.Text = fmt.Sprintf(UTCOffsetSetStr, FormatUTCOffset(prevOffset), FormatUTCOffset(utcOffset))
+			// 			}
+			// 		}
+			// 	default:
+			// 		msg.Text = usageStr
+			// 	}
+			// }
 		default:
 			continue
 		}
 
-		_, err = bot.Send(msg)
+		sentMsg, err := bot.Send(msg)
 		if err != nil {
 			log.Printf("Send message error: %s", err)
+		}
+		if savingMessageID {
+			data.Users[idx].MenuMessageID = sentMsg.MessageID
+			err := SaveData(dataPath, data)
+			if err != nil {
+				log.Printf("Error saving Message ID: %s", err)
+			}
 		}
 	}
 }
@@ -331,4 +360,5 @@ const (
 	WBTimerStr         = "Alarm | `%s`"
 	WBTimerChangedStr  = "Alarm | `%s` \u2794 `%s`"
 	WBAlarmStr         = "*%s* | `%s`"
+	MainMenuStr        = "Diabler"
 )
